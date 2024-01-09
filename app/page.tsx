61fbbabae81af1379373c0c1bc8c7962ff426b2c
Tatiana Toro Giraldo <tatianatorog@gmail.com>
2024-01-09 11:33:44 -0500

    addING PROPS

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>hola</h1>
-      <RandomFox />
+      <RandomFox image={`https://randomfox.ca/images/${Math.floor(Math.random() * 122) + 1}.jpg`} />
     </main>
   )
 }
