a1966478af0b99c3d27f7d02fbd187f415991067
Tatiana Toro Giraldo <tatianatorog@gmail.com>
2024-01-09 11:21:14 -0500

    add first component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from 'next/image'
+import { RandomFox } from "@/components/RandomFox"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>hola</h1>
+      <RandomFox />
     </main>
   )
 }
