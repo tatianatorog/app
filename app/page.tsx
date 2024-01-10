96d7d94b7e863380f1f321dd34dd22c8f6a4277c
Tatiana Toro Giraldo <tatianatorog@gmail.com>
2024-01-09 20:03:51 -0500

    addING hooks

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,31 @@
+'use client';
 import { RandomFox } from "@/components/RandomFox"
 
+import React, { useState } from 'react';
+
+const random = () => Math.floor(Math.random() * 123) + 1;
+
+const generateId = () => Math.random().toString(36).substring(2);
+
+type ImageItems = Array<{ id: string; url: string }>;
+
 export default function Home() {
+  const [images, setImages] = useState<ImageItems>([
+    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
+    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
+    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
+    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
+  ]);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>hola</h1>
-      <RandomFox image={`https://randomfox.ca/images/${Math.floor(Math.random() * 122) + 1}.jpg`} />
+      {images.map(({ id, url }) => (
+        <div key={id} className="p-4">
+          <RandomFox image={url} />
+        </div>
+      ))}
     </main>
-  )
+  );
 }
+        
