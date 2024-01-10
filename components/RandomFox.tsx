68ebb264c1ad2f80359b6b2e4a0637b615155355
Tatiana Toro Giraldo <tatianatorog@gmail.com>
2024-01-10 12:23:48 -0500

    adding useRef

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,17 +1,35 @@
+import { useRef } from "react";
 
 type Props = { image: string };
 
+export const RandomFox = ({ image }: Props): JSX.Element => {
+    const node = useRef<HTMLImageElement>(null);
 
-const random = () => Math.floor(Math.random() * 123) + 1;
-
-export const RandomFox = ({ image }: Props):JSX.Element => {
-
-    // Rest of your code...
-
-    return <img width={320} height="auto" src={image} className="rounded"></img>;
+    return (
+        <img
+            ref={node}
+            width={320}
+            height="auto"
+            src={image}
+            className="rounded"
+        />
+    );
 };
+
+
 
 // Definir los props en react consiste en definir el 
 // tipo de dato que va a recibir el componente dentro de un objeto 
 // que se le pasa como parametro a la funcion que define el componente
 // En este caso el componente RandomFox recibe un objeto de tipo Props
+
+// Tipos para referencias y observadores
+// Cuando trabajamos con React y TypeScript, puede que nos encontremos con problemas a la hora de utilizar referencias y observadores en nuestros componentes. Esto se debe a que, dependiendo del objeto HTML con el que estemos trabajando, necesitamos tener en cuenta ciertos conceptos para que nuestro código sea válido.
+
+// Digamos que vamos a nuestro componente RandomFox y queremos añadir una referencia con useRef().
+
+// Incluso si especificamos que la referencia será de tipo HTMLImageElement en el generic de useRef(), el error persiste:
+
+// Para solucionar este problema, debemos inicializar la referencia con un valor nulo en lugar de dejarla sin asignar:
+
+
